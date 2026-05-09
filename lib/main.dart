cc5a6bcef5633627f441fd8500ee79b0f53ddd38
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-09 03:12:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ class AppConfig {
   static const int copyrightYear = 2026;
   static const String supabaseUrl = 'https://jmsmrojtlstppnpwmkkk.supabase.co';
   static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos';
-}
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos'
 
 class AppColors {
   static const Color backgroundStart = Color(0xFFCAD5D4);
