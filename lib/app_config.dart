a864b8a3fab2a8ff9d6ebbfe6b23495253d09d0e
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-09 13:01:45 +0300

    Update app_config.dart

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,8 +1,7 @@
 class AppConfig {
-  static const String appName = 'SeaChat';
-  static const String copyrightName = 'Joud Oday';
-  static const int copyrightYear = 2026;
+  // تأكد مافي / بالنهاية
   static const String supabaseUrl = 'https://jmsmrojtlstppnpwmkkk.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos';
+  
+  // هذا لازم يبدأ بـ eyJ
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos';
 }
