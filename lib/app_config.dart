ef4d467da0b928ac6f48009be40cc06473e9f80b
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-09 04:21:09 +0300

    Create app_config.dart

---
+++ lib/app_config.dart
@@ -0,0 +1,8 @@
+class AppConfig {
+  static const String appName = 'SeaChat';
+  static const String copyrightName = 'Joud Oday';
+  static const int copyrightYear = 2026;
+  static const String supabaseUrl = 'https://jmsmrojtlstppnpwmkkk.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos';
+}
