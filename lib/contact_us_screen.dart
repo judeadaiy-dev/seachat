b4dba3fad69ed58d90bba023c8b424db9c2bc49d
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-11 02:42:36 +0300

    Update contact_us_screen.dart

--- lib/contact_us_screen.dart
+++ lib/contact_us_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'main.dart';
-
+import 'widgets.dart';
 class ContactUsScreen extends StatefulWidget {
   const ContactUsScreen({super.key});
 
