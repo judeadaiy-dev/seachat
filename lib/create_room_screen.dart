02d0bb4c8f43df8ac7c6906b40953fa85382cbc8
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-11 02:43:15 +0300

    Update create_room_screen.dart

--- lib/create_room_screen.dart
+++ lib/create_room_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'main.dart';
-
+import 'widgets.dart';
 class CreateRoomRequestScreen extends StatefulWidget {
   const CreateRoomRequestScreen({super.key});
 
