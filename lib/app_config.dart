0a7682209d1c36be37206c59a7791fb96cdebd41
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-13 12:57:58 +0300

    Delete lib/app_config.dart

--- lib/app_config.dart
+++
@@ -1,7 +0,0 @@
-class AppConfig {
-  // تأكد مافي / بالنهاية
-  static const String supabaseUrl = 'https://jmsmrojtlstppnpwmkkk.supabase.co';
-  
-  // هذا لازم يبدأ بـ eyJ
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc21yb2p0bHN0cHBucHdta2trIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTg2NDAsImV4cCI6MjA4ODM5NDY0MH0.j7gxr5CvrfvbJJzK_pMwVHiCE2AqpXUTThpeLEBmsos';
-}
